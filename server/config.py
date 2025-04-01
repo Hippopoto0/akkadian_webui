03c96ffe62b8b45735cfeb3ad22f6b585f09a625
Hippopoto0 <67485469+Hippopoto0@users.noreply.github.com>
2025-04-01 14:14:31 +0100

    added cors origins as env, should make a local and prod version, now requires CORS_ORIGINS to  be set when deploying

--- server/config.py
+++ server/config.py
@@ -1,6 +1,7 @@
 # config.py
 from pydantic_settings import BaseSettings
 import os
+import json
 
 class Settings(BaseSettings):
     """
@@ -12,4 +13,26 @@ class Settings(BaseSettings):
     device: str = "cuda" if os.getenv("CUDA_AVAILABLE") else "cpu" # Automatically use CUDA if available
 
     class Config:
-        env_file = ".env" # Load environment variables from .env file
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings,
+            env_settings,
+            file_secret_settings,
+        ):
+            # Check if CORS_ORIGINS is already set as an environment variable
+            cors_origins_env = os.getenv("CORS_ORIGINS")
+            if cors_origins_env:
+                try:
+                    cors_origins_list = json.loads(cors_origins_env)
+                    env_settings.cors_origins = cors_origins_list
+                except json.JSONDecodeError:
+                    print("Warning: Could not parse CORS_ORIGINS environment variable as a JSON list.")
+            return (
+                init_settings,
+                env_settings,
+                file_secret_settings,
+            )
